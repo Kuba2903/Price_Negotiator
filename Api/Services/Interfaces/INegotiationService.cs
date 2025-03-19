333a213191a73dc82414cbcba61a5fd182877edd
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2025-03-19 17:12:23 +0100

    create interfaces for product and negotiation services

---
+++ Api/Services/Interfaces/INegotiationService.cs
@@ -0,0 +1,12 @@
+﻿using Api.DTO_s;
+using Data.Entities;
+
+namespace Api.Services.Interfaces
+{
+    public interface INegotiationService
+    {
+        Task<Negotiation> CreateProposal(PriceProposalDTO proposalDto);
+        Task<Negotiation> RespondToProposal(int negotiationId, bool accept);
+        Task<IEnumerable<Negotiation>> GetPendingNegotiations();
+    }
+}
---
+++ Api/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Api.DTO_s;
+using Data.Entities;
+
+namespace Api.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<Product> AddProduct(CreateProductDTO productDto);
+        Task<Product> GetProduct(int id);
+        Task<IEnumerable<Product>> GetAllProducts();
+    }
+}
