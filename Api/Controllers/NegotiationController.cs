4371c95fd07c1ecc9fd058ebf9ba3c29f32ead3f
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2025-03-20 09:39:28 +0100

    create negotation controller

--- Api/Controllers/NegotiationController.cs
+++ Api/Controllers/NegotiationController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api.DTO_s;
+using Api.Services.Interfaces;
+using Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -7,6 +11,32 @@ namespace Api.Controllers
     [ApiController]
     public class NegotiationController : ControllerBase
     {
+        private readonly INegotiationService _negotiationService;
 
+        public NegotiationController(INegotiationService negotiationService)
+        {
+            _negotiationService = negotiationService;
+        }
+
+        [HttpPost("proposals")]
+        public ActionResult<Negotiation> CreateProposal(PriceProposalDTO proposalDto)
+        {
+            var negotiation = _negotiationService.CreateProposal(proposalDto);
+            return CreatedAtAction(nameof(GetPendingNegotiations), new { id = negotiation.Id }, negotiation);
+        }
+
+        [HttpPost("proposals/{id}/respond")]
+        //[Authorize(Roles = "Employee")]
+        public ActionResult<Negotiation> RespondToProposal(int id, bool accept)
+        {
+            return _negotiationService.RespondToProposal(id, accept);
+        }
+
+        [HttpGet("pending")]
+        //[Authorize(Roles = "Employee")]
+        public async Task<ActionResult<IEnumerable<Negotiation>>> GetPendingNegotiations()
+        {
+            return Ok(await _negotiationService.GetPendingNegotiations());
+        }
     }
 }
